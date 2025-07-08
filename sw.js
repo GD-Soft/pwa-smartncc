cb0fa6964423e63ca27db5f6cc84f87e98b25805
Gianka78 <info@gdinformatica.it>
2025-07-08 09:22:58 +0200

    Avoid duplicate notifications

--- sw.js
+++ sw.js
@@ -47,18 +47,6 @@ self.addEventListener('fetch', event => {
   );
 });
 
-self.addEventListener('push', event => {
-  let data = { title: 'SmartNCC', body: 'Push message received.' };
-  if (event.data) {
-    data = event.data.json();
-  }
-  const options = {
-    body: data.body,
-    icon: 'https://demo2018prod.smartncc.it/pwa-smartncc/icon-192.png',
-    badge: 'https://demo2018prod.smartncc.it/pwa-smartncc/icon-192.png'
-  };
-  event.waitUntil(self.registration.showNotification(data.title, options));
-});
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
@@ -76,10 +64,14 @@ self.addEventListener('notificationclick', event => {
   );
 });
 
-messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification && payload.notification.title ? payload.notification.title : 'SmartNCC';
+messaging.onBackgroundMessage(payload => {
+  if (payload.notification) {
+    // Let Firebase handle display if a notification payload is present
+    return;
+  }
+  const notificationTitle = payload.data && payload.data.title ? payload.data.title : 'SmartNCC';
   const notificationOptions = {
-    body: payload.notification && payload.notification.body,
+    body: payload.data && payload.data.body,
     icon: 'https://demo2018prod.smartncc.it/pwa-smartncc/icon-192.png',
     badge: 'https://demo2018prod.smartncc.it/pwa-smartncc/icon-192.png'
   };
